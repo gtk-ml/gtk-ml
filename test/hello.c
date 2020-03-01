7ae0db03d9f4c059bf15b4e5f69ef467c8150eea
Szymon Walter <waltersz@protonmail.com>
2020-03-01 10:42:19 +0100

    move gui.gtkml to a seperate file

--- test/hello.c
+++ test/hello.c
@@ -1,23 +1,16 @@
 #include <gtk/gtk.h>
 #include "gtk-ml.h"
 
-static char *GUI =
-    "  ; define a Window macro\n"
-    "  (define-macro (Window title width height ...body)\n"
-    "    (lambda (app)\n"
-    "      (new-window app title width height)\n"
-    "      ...body))\n"
-    "  ; run the application\n"
-    "  (Application \"de.walterpi.example\" flags-none {\n"
-    "    :activate (Window \"gtk-ml example\" 640 480)})\n";
+#define GUI "examples/gui.gtkml"
 
 int main() {
     const char *err;
 
     GtkMl_Context *ctx = gtk_ml_new_context();
 
+    char *src;
     GtkMl_S *gui;
-    if (!(gui = gtk_ml_loads(ctx, &err, GUI))) {
+    if (!(gui = gtk_ml_load(ctx, &src, &err, GUI))) {
         gtk_ml_del_context(ctx);
         fprintf(stderr, "%s\n", err);
         return 1;
@@ -37,6 +30,7 @@ int main() {
     int status = g_application_run(G_APPLICATION(app->value.s_userdata.userdata), 0, NULL);
 
     gtk_ml_del_context(ctx);
+    free(src);
 
     return status;
 }
